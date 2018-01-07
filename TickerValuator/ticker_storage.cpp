b57ea063710bd24a78425d5ba88f2e0edee5e24c
Paulo Chang <4237241+paulochang@users.noreply.github.com>
2018-01-07 13:34:48 +0100

    add ticker sort and classifier

--- TickerValuator/ticker_storage.cpp
+++ TickerValuator/ticker_storage.cpp
@@ -3,11 +3,35 @@
 //
 
 #include "ticker_storage.h"
+
+struct Comp
+{
+    bool operator() ( const ticker& s, std::string i )
+    {
+        return s.getSymbol_() < i;
+    }
+
+    bool operator() ( std::string i, const ticker& s )
+    {
+        return i < s.getSymbol_();
+    }
+};
+
 void ticker_storage::add_ticker(ticker myTicker) {
-    ticker_set_.push_back(myTicker);
-    symbol_set_.insert(myTicker.get_symbol());
+    is_sorted = false;
+    ticker_vector_.push_back(myTicker);
+    symbol_set_.insert(myTicker.getSymbol_());
 }
 
 void ticker_storage::sort_ticker() {
-    std::sort(ticker_set_.begin(), ticker_set_.end());
+    std::sort(ticker_vector_.begin(), ticker_vector_.end());
+    is_sorted = true;
+}
+
+void ticker_storage::classify() {
+    if (!is_sorted) sort_ticker();
+    for (auto i : symbol_set_) {
+        classifying_map_.insert(make_pair(i,std::equal_range(ticker_vector_.begin(),ticker_vector_.end(),i, Comp())));
+    }
+    is_classified = true;
 }
