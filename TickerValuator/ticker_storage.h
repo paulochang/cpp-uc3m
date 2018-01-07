ce01ec53bd7266ff4cfdf8a0aefb68698abe7af9
Eddy Castro <11904270+eddyq7i@users.noreply.github.com>
2018-01-08 00:36:40 +0100

    Creating parser object, starting to analyze input arguments.

---
+++ TickerValuator/ticker_storage.h
@@ -0,0 +1,28 @@
+//
+// Created by Paulo Chang on 1/6/18.
+//
+
+#ifndef TICKERVALUATOR_TICKER_STORAGE_H
+#define TICKERVALUATOR_TICKER_STORAGE_H
+
+#include <vector>
+#include <unordered_set>
+#include <string>
+#include <unordered_map>
+#include "ticker.h"
+
+class ticker_storage {
+public:
+    void add_ticker(ticker myTicker);
+    void sort_ticker();
+    void classify();
+
+private:
+    std::vector<ticker> ticker_vector_;
+    std::unordered_set<std::string> symbol_set_;
+    std::unordered_map<std::string, std::pair<std::vector<ticker>::iterator,std::vector<ticker>::iterator>> classifying_map_;
+    bool is_sorted {false};
+    bool is_classified {false};
+};
+
+#endif //TICKERVALUATOR_TICKER_STORAGE_H
