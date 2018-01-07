f3ebdbb8832b5f6256dc238d75d2b5815f0e5ea7
Eddy Castro <cas11032@uvg.edu.gt>
2018-01-07 12:13:36 +0100

    Creating ticker objects based on input data.

--- TickerValuator/input.cpp
+++ TickerValuator/input.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <string>
 
+#include "ticker.h"
+
 using namespace std;
 
 int main () {
@@ -18,6 +20,16 @@ int main () {
         cout << "symbol: " << line.substr(line.find('"')+1, line.find('"', line.find('"')+1) - line.find('"')-1) << '\n';
         cout << "price: " << line.substr(line.find('"', line.find('"')+1)+2) << '\n';
         cout << "line:" << line << '\n';
+
+        int date = stoi(line.substr(0,2) + line.substr(3,2) + line.substr(6,4));
+        int time = stoi(line.substr(11,2) + line.substr(14,2));
+        double seconds = stod(line.substr(17, 9));
+        string symbol = line.substr(line.find('"')+1, line.find('"', line.find('"')+1) - line.find('"')-1);
+        double price = stod(line.substr(line.find('"', line.find('"')+1)+2));
+
+
+        ticker(date, time, seconds, symbol, price);
+
     }
     myfile.close();
   }
