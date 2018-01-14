a951f7705202eca17eded0287adc40331b768a44
paulochang <paulochang@128.1.168.192.in-addr.arpa>
2018-01-14 06:51:32 +0100

    Add a simplified ticker structure for final ticker printing

---
+++ TickerValuator/simplified_ticker.h
@@ -0,0 +1,14 @@
+//
+// Created by Paulo Chang on 1/14/18.
+//
+
+#ifndef TICKERVALUATOR_SIMPLIFIED_TICKER_H
+#define TICKERVALUATOR_SIMPLIFIED_TICKER_H
+
+
+class simplified_ticker {
+
+};
+
+
+#endif //TICKERVALUATOR_SIMPLIFIED_TICKER_H
