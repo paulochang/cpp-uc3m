7fb11b6941c12ae9af004d4338a6223b2593b063
Paulo Chang <4237241+paulochang@users.noreply.github.com>
2018-01-14 06:50:54 +0100

    Add demo ticker objects to try sort Optimize for-loop const usage to avoid unnecessary copying Clarify iterators usage

--- TickerValuator/main.cpp
+++ TickerValuator/main.cpp
@@ -10,30 +10,42 @@ int main() {
     cout << __cplusplus << endl;
 
     ticker_storage ts = ticker_storage();
-    ticker t1 = ticker(1012001,1200,23.3242,"APPL", 803.20);
-    ticker t2 = ticker(1012000,1200,23.3242,"APPL", 803.20);
-    ticker t3 = ticker(1012000,1200,23.3242,"GOOGL", 803.20);
-    ts.add_ticker(t1);
-    ts.add_ticker(t2);
-    ts.add_ticker(t3);
-    ts.sort_ticker();
+    ticker a1 = ticker(1012001, 1200, 23.3242, "APPL", 803.20);
+    ticker a2 = ticker(1012000, 1200, 23.3242, "APPL", 803.20);
+    ticker a3 = ticker(1012000, 1201, 23.3242, "APPL", 803.20);
+    ticker a4 = ticker(1012000, 1202, 23.3242, "APPL", 803.20);
+    ticker a5 = ticker(1012000, 1202, 23.3042, "APPL", 803.20);
+    ticker g1 = ticker(1012000, 1200, 23.3242, "GOOGL", 803.20);
+    ts.add_ticker(a1);
+    ts.add_ticker(a2);
+    ts.add_ticker(a5);
+    ts.add_ticker(a4);
+    ts.add_ticker(a3);
+    ts.add_ticker(g1);
 
-    std::cout << "Sorted Ticker vector:" << endl;
-    for (auto i: ts.getTicker_vector_()) {
-        std::cout << i.to_string() << endl;
+    std::cout << "Sorted ticker vector:" << endl;
+    for (const auto &i: ts.ticker_vector()) {
+        std::cout << i << endl;
     }
 
-    ts.classify();
+    ts.symbol_classify();
 
 
-    std::cout << "Holi: " << t1 << endl;
-    std::cout << "Ticker classifying map:" << endl;
+    std::cout << "Holi: " << a1 << endl;
+    std::cout << "ticker classifying map:" << endl;
 
 
-    for(auto i : ts.getClassifying_map_()) {
-        for ( auto it = i.second.first; it != i.second.second; ++it )
-            std::cout << " " << it.base()->to_string() << endl;
+    for (auto current_symbol : ts.classifying_map()) {
+        std::cout << "Current group: " << current_symbol.first << endl;
+        auto first_ticker_it = current_symbol.second.first;
+        auto last_ticker_it = current_symbol.second.second;
+        auto current_ticker_it = first_ticker_it;
+        while (current_ticker_it != last_ticker_it) {
+            std::cout << " " << (*current_ticker_it) << endl;
+            current_ticker_it++;
+        }
     }
+
 
     return 0;
 }
