67e9d9b299ded9daaff777ae0f802286a184962c
Paulo Chang <4237241+paulochang@users.noreply.github.com>
2018-01-14 08:44:39 +0100

    Add simplified ticker representation

--- TickerValuator/simplified_ticker.cpp
+++ TickerValuator/simplified_ticker.cpp
@@ -3,3 +3,9 @@
 //
 
 #include "simplified_ticker.h"
+
+simplified_ticker::simplified_ticker(unsigned int date, unsigned short time, double price) {
+    date_ = date;
+    time_ = time;
+    price_ = price;
+}
