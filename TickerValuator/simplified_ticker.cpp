6a6a77d3f477d48f0e5afc8e5bb0fe361fedf7b5
paulochang <paulochang@NonePro.local>
2018-01-21 00:56:39 +0100

    Add leap year validation

--- TickerValuator/simplified_ticker.cpp
+++ TickerValuator/simplified_ticker.cpp
@@ -27,6 +27,9 @@ void simplified_ticker::write(fmt::MemoryWriter &out) {
 
     using namespace std;
 
+    unsigned int max_days[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    bool leap_year = false;
+
     unsigned int date = date_;
     
     unsigned short year = date/10000;
@@ -39,6 +42,29 @@ void simplified_ticker::write(fmt::MemoryWriter &out) {
 
     unsigned short hour = my_time / 100;
     my_time -= hour * 100;
+
+    if (time_ > 2359 or time_ % 100 > 59) {
+        cerr << fmt::format("Invalid value for my_time: {:02d}:{:02d}\n", hour, my_time);
+    }
+
+    if (month > 12 or date > max_days[month - 1]) {
+        cerr << fmt::format("Invalid value for date: {:02d}-{:02d}-{:02d}\n", date, month, year);
+    }
+
+    if (month == 2) {
+        if (year % 4 != 0) {
+            leap_year = false;
+        } else if (year % 100 != 0) {
+            leap_year = true;
+        } else if (year % 400 != 0) {
+            leap_year = false;
+        }
+
+        if (date == 29 and !leap_year) {
+            cerr << fmt::format("Invalid leap year:: {:02d}-{:02d}-{:02d}\n", date, month, year);
+        }
+    }
+    
 
     out.write("{:02d}-{:02d}-{:02d} {:02d}:{:02d} {:.2f}\n", date, month, year, hour, my_time, price_);
 }
