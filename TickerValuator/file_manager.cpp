a6243281bc1f5d3ba947c5d4869bb341be231fc7
Paulo Chang <4237241+paulochang@users.noreply.github.com>
2018-01-25 06:38:27 +0100

    Refactor io operations

--- TickerValuator/file_manager.cpp
+++ TickerValuator/file_manager.cpp
@@ -1,43 +1,97 @@
 
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+
+#include <regex>
+#include <sstream>
 #include "file_manager.h"
 #include "fmt/format.h"
 
 using namespace std;
 
 
-ticker_storage file_manager::file_reader(string input_path){
+ticker_storage file_manager::file_reader(const string input_path) {
 
     string line;
     ticker_storage ts = ticker_storage();
 
-    ifstream myfile (input_path);
+    ifstream myfile(input_path);
+
+
+    //long counter = 0;
 
     // Open file
-    if (myfile.is_open()) {
+    /*if (myfile.is_open()) {
 
         cin.exceptions(cin.exceptions() | ios_base::failbit);
 
         ticker tk;
+        //unsigned int d;
+        //unsigned int m;
+        //unsigned int y;
+        //unsigned int h;
+        //unsigned int min;
+        //double secs;
+        //char symbol[32];
+        //double price;
+        //13-02-2002 18:58:19.594486 "BBVA" 184.18
+        // while(int ret = fscanf(fp, "%u-%u-%u %u:%u:%lf \"%[^\"]\" %lf",
+        //                      &d, &m, &y, &h, &min, &secs, symbol, &price) == 8) {
 
-        while ( myfile >> tk){
+        while (myfile >> tk) {
             // DEBUG
-            // cout << "Ticker cout: " << tk << endl;
+            // cout << "counter: " << counter++;
+            // unsigned int date = d + m * 100u + y * 10000u;
+            //unsigned short time_ = static_cast<unsigned short>(h * 100u + min);
+            //tk = ticker{date, time_, secs, symbol, price};
+
             ts.add_ticker(tk);
+
+            //cout << "Ticker cout: " << tk << endl;
         }
         myfile.close();
 
     }
 
+        //fclose(fp);
+
     else cout << "Unable to open file " << input_path << endl;
-    
+*/
+    //////
+
+
+    FILE *fp;
+
+    fp = fopen(input_path.c_str(), "r");
+    ticker tk;
+    unsigned int d;
+    unsigned int m;
+    unsigned int y;
+    unsigned int h;
+    unsigned int min;
+    double secs;
+    char symbol[32];
+    double price;
+    //13-02-2002 18:58:19.594486 "BBVA" 184.18
+    while (int ret = fscanf(fp, "%u-%u-%u %u:%u:%lf \"%[^\"]\" %lf",
+                            &d, &m, &y, &h, &min, &secs, symbol, &price) == 8) {
+
+        unsigned int date = d + m * 100u + y * 10000u;
+        unsigned short time_ = static_cast<unsigned short>(h * 100u + min);
+        tk = ticker{date, time_, secs, symbol, price};
+        ts.add_ticker(tk);
+    }
+
+    fclose(fp);
+
+    /////
     return ts;
 
 }
 
 void file_manager::file_writer(const string output_path, const string file_name,
-                               const std::vector<simplified_ticker> printing_vector) const {
-
-    stringstream result;
+                               const std::vector<simplified_ticker> &printing_vector) const {
 
     std::string my_string(file_name);
 
@@ -46,7 +100,7 @@ void file_manager::file_writer(const string output_path, const string file_name,
     my_string.erase(std::remove_if(my_string.begin(), my_string.end(), [](char c) { return !std::isalnum(c); }),
                     my_string.end());
 
-    ofstream myfile (output_path+my_string+".txt");
+    auto myfile = std::fstream(output_path + my_string + ".txt", std::ios::out | std::ios::binary);
 
     fmt::MemoryWriter writer;
 
@@ -57,7 +111,8 @@ void file_manager::file_writer(const string output_path, const string file_name,
             stk.write(writer);
         
         }
-        myfile << writer.c_str();
+        //myfile << writer.c_str();
+        myfile.write(writer.c_str(), writer.size());
         myfile.close();
     }
 
--- TickerValuator/file_manager.h
+++ TickerValuator/file_manager.h
@@ -1,11 +1,5 @@
 
 #include <string>
-#include <iostream>
-#include <fstream>
-
-#include <regex>
-#include <sstream>
-#include <locale>
 
 #include "ticker.h"
 #include "ticker_storage.h"
@@ -15,9 +9,9 @@ using namespace std;
 class file_manager {
 
     public:
-    ticker_storage file_reader(const string input_path);
+    ticker_storage file_reader(string input_path);
 
-    void file_writer(const string output_path, const string file_name,
-                     const std::vector<simplified_ticker> printing_vector) const;
+    void file_writer(string output_path, string file_name,
+                     const std::vector<simplified_ticker> &printing_vector) const;
 };
 
