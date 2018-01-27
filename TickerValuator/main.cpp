8eb3d1fee94ae2045a2d3cae3bb732a8c028e82c
Paulo Chang <4237241+paulochang@users.noreply.github.com>
2018-01-27 22:20:00 +0100

    Remove debugging code.

--- TickerValuator/main.cpp
+++ TickerValuator/main.cpp
@@ -18,8 +18,6 @@ typedef tbb::concurrent_unordered_map<std::string, std::pair<tbb::concurrent_vec
 string output_path;
 unsigned long max_size;
 
-constexpr bool DEBUG_ = true;
-
 /// Manual ticker insertion
 /// \return ts a ticker_storage object
 ticker_storage manual_ticker_insertion() {
@@ -81,11 +79,12 @@ tbb::concurrent_vector<simplified_ticker> get_print_ready_vector(unsigned long m
     while (current_ticker_it != last_ticker_it) {
         ticker current_ticker = (*current_ticker_it);
         ticker previous_ticker = (*previous_ticker_it);
+        ticker next_ticker = (*next_ticker_it);
 
         // CASE1: next ticker has same timestamp (and symbol) as previous
-        if (next_ticker_it != last_ticker_it && current_ticker == (*next_ticker_it)) {
+        if (next_ticker_it != last_ticker_it && current_ticker == next_ticker) {
             // Add price to pricing set (this will average later)
-            (*next_ticker_it).add_price(current_ticker.price());
+            next_ticker.add_price(current_ticker.price(), current_ticker.price_nr());
             current_ticker_it = next_ticker_it++;
         } else {
 
@@ -112,7 +111,7 @@ tbb::concurrent_vector<simplified_ticker> get_print_ready_vector(unsigned long m
             //endregion
 
             //region ** Area to the right calculation **
-            if (next_ticker_it != last_ticker_it && AreaUtils::isContiguous(current_ticker, (*next_ticker_it))) {
+            if (next_ticker_it != last_ticker_it && AreaUtils::isContiguous(current_ticker, next_ticker)) {
                 //CASE 2.a: if there is at least one remaining ticker
                 // AND it's in the same timeframe as current one
                 // enable previous_in_range flag
@@ -180,41 +179,18 @@ void print_processed_list(const ticker_storage &ts) {
 
     MySymbolMap::range_type r = mySymbolMap.range();
 
-
     tbb::parallel_for(r, SymbolMapIteration(mySymbolMap), tbb::auto_partitioner());
-    // For each asset...
-    /*
-    for (auto current_symbol : ts.classifying_map()) {
-
-        auto first_ticker_it = current_symbol.second.first;
-        auto last_ticker_it = current_symbol.second.second;
-
-        tbb::concurrent_vector<simplified_ticker> printing_vector = get_print_ready_vector(max_size, first_ticker_it,
-                                                                                           last_ticker_it);
-        fm.file_writer(output, current_symbol.first, printing_vector);
-    }
-     */
 }
 
 int main(int argc, char **argv) {
     std::chrono::time_point<std::chrono::high_resolution_clock> time_start = std::chrono::high_resolution_clock::now();
 
     using namespace std;
-
-    ios_base::sync_with_stdio(false);
-
-    char buffer[1048576];
-    std::cin.rdbuf()->pubsetbuf(buffer, sizeof(buffer));
-
-    cin.tie(nullptr);
-
     string filename;
     file_manager fm = file_manager();
 
     // INITIALIZE TICKER_STORAGE
     ticker_storage ts = ticker_storage();
-
-    auto time_initialize_variables = std::chrono::high_resolution_clock::now();
 
     //region **Argument Handling**
     // Handle different arg numbers
@@ -245,9 +221,6 @@ int main(int argc, char **argv) {
         }
     }
     //endregion
-    if (DEBUG_) {
-    std::cout << "initialized. " << endl;
-    }
 
     if (!output_path.empty()) {
 
@@ -264,56 +237,21 @@ int main(int argc, char **argv) {
     } else {
         ts = fm.file_reader(filename);
     }
-    if (DEBUG_) {
-       std::cout << "Read file " << endl;
-    }
-    auto time_read_file = std::chrono::high_resolution_clock::now();
 
     // ** Process Ticker List **
     // 1. Sort ticker based on symbol, date and time
     ts.sort_ticker();
-if (DEBUG_) {
-    std::cout << "sorted vector" << endl;
-}
-    auto time_sort = std::chrono::high_resolution_clock::now();
-    //region DEBUG - DISPLAY SORTED VECTOR
-    /*
-    std::cout << "Sorted ticker vector:" << endl;
-    for (const auto &i: ts.ticker_vector()) {
-        std::cout << i << endl;
-    }
-     */
-    //endregion
 
     // 2. Classify based on symbol
     // Final structure is:
     // Map<Symbol, { beginning iterator, end iterator } >
     ts.symbol_classify();
-if (DEBUG_) {
-    std::cout << "classified vector" << endl;
-}
-    auto time_classify = std::chrono::high_resolution_clock::now();
 
     // 3. Initialize vector to print ticker's
-
     // 4. calculate areas
-    //region DEBUG - TICKER CLASSIFYING MAP
-    //std::cout << "ticker classifying map:" << endl; 
-    //endregion
-
     print_processed_list(ts);
-if (DEBUG_) {
-    std::cout << "printed vector" << endl;
-}
+    
     auto time_print_file = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration_initialize = time_initialize_variables - time_start;
-    std::cout << "Initialize time: " << duration_initialize.count() << " s\n";
-    std::chrono::duration<double> duration_read = time_read_file - time_start;
-    std::cout << "Read time: " << duration_read.count() << " s\n";
-    std::chrono::duration<double> duration_sort = time_sort - time_start;
-    std::cout << "Sort time: " << duration_sort.count() << " s\n";
-    std::chrono::duration<double> duration_classify = time_classify - time_start;
-    std::cout << "Classify time: " << duration_classify.count() << " s\n";
     std::chrono::duration<double> duration_print_time = time_print_file - time_start;
     std::cout << "Print time: " << duration_print_time.count() << " s\n";
 
